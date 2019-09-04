a0ca45be0259cb270063a3ee95b12170b40adcd8
thaison <thaison113@hotmail.com>
2019-09-04 17:39:27 +0700

    update

--- cms/modules/Cockpit/webhooks.php
+++ cms/modules/Cockpit/webhooks.php
@@ -48,7 +48,7 @@ foreach ($webhooks as &$webhook) {
                 }
 
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
                 curl_setopt($ch, CURLOPT_TIMEOUT, 1);
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
